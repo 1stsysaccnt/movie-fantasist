ec202cf28b060d8f34a656f4d4c43241beb35028
1stsysaccnt <1st.sysaccnt@gmail.com>
2025-04-17 21:46:12 +0800

    Update home.js

--- my-website/my-website/js/home.js
+++ my-website/my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'e82a92ea046bc3181607bd16e159379c';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/w500';
     let currentItem;
